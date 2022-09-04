1ddb67ccb3f114d9c8300822fb249184b42103d8
mijia <1137551712@qq.com>
2022-09-04 09:08:28 +0800

    22/9/4

---
+++ 160.相交链表.java
@@ -0,0 +1,103 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=160 lang=java
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // 暴力
+    // public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //     for(ListNode node1 = headA ;node1!=null;node1 =node1.next){
+    //         for(ListNode node2 = headB ;node2!=null;node2 = node2.next){
+    //             if(node1==node2){
+    //                return node2;
+    //             }
+    //         }
+    //     }
+    //     return null;
+    // }
+
+
+    /*
+     * 双指针 追击：“两个有关的人即使刚开始错过了，到后面还是可以相遇”
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null){
+            return null;
+        }
+        ListNode ptrA=headA,ptrB=headB;
+        while ( ptrA != ptrB){
+    
+            if (ptrA == null){
+                ptrA=headB;
+            }else{
+                ptrA=ptrA.next;
+            }
+            if (ptrB == null){
+                ptrB=headA;
+            }else{
+                ptrB=ptrB.next;
+            }    
+        }
+        // 不相交那么都是最后是null 同样返回ptrA就行  
+        return ptrA;
+    }
+
+    // 简洁一点
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null){
+            return null;
+        }
+        ListNode ptrA=headA,ptrB=headB;
+        while ( ptrA != ptrB){
+            ptrA = ptrA == null ? headB : ptrA.next;
+            ptrB = ptrB == null ? headA : ptrB.next;
+        }
+        return ptrA;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int la=0,lb=0,diff=0;
+        ListNode pa=headA,pb=headB;
+        while(pa!=null){la++;pa=pa.next;}
+        while(pb!=null){lb++;pb=pb.next;}
+        if (la>lb){
+            diff=la-lb;
+            // 我们将 pa 用来记录长的
+            pa = headA;
+            pb = headB;
+        }else{
+            diff=lb-la;
+            pa = headB;
+            pb = headA;
+        }
+        while(diff!=0){
+            pa=pa.next;
+            diff--;
+        }
+        while(pa != null && pb != null){
+            if (pa==pb){
+                return pa;
+            }
+            pa=pa.next;
+            pb=pb.next;
+        }
+        return null;
+    }
+}
+// @lc code=end
+
---
+++ 234.回文链表.java
@@ -0,0 +1,67 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode.cn id=234 lang=java
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    /*
+     * 时间O(n) 空间O(1)
+     * 用到翻转列表的方法，注意奇数个的时候,slowPtr舍去中间值，跳过)
+     * 即slowPtr=slowPtr.next
+     */
+    public boolean isPalindrome(ListNode head) {
+        ListNode slowPtr = head , fastPtr = head ;
+        /*
+         *  偶数的时候，fastPtr 指向null 退出
+         *  奇数的时候，fastPtr.next=null 退出
+         */
+        while (fastPtr != null && fastPtr.next != null){
+            fastPtr = fastPtr.next.next ;
+            slowPtr = slowPtr.next ;
+        }
+        // 奇数个的时候slowPtr 跳过中间的那个 向后移动一个
+        if (fastPtr != null ){
+            slowPtr = slowPtr.next ;
+        }
+
+        // 从slowPtr开始的链表进行反转，然后再去和head开始比较
+        slowPtr = reverseList(slowPtr);
+        fastPtr = head ;
+        while (slowPtr != null){
+            if (slowPtr.val != fastPtr.val){
+                return false ;
+            }
+            slowPtr = slowPtr.next ;
+            fastPtr = fastPtr.next ;
+        }
+        return true ;
+    }
+
+    public ListNode reverseList(ListNode head){
+        // 递归结束条件 head.next = null
+        if (head == null || head.next == null){
+            return head ;
+        }
+        ListNode newNode = reverseList(head.next) ;
+        head.next.next = head ;
+        head.next = null ;
+        return newNode ;
+    }
+}
+// @lc code=end
+
