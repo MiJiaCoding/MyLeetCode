a41a6e18cf577e15e013a4a7dd5c6a1c778e200b
mijia <1137551712@qq.com>
2022-10-02 17:10:44 +0800

    10/2/22

---
+++ 128.最长连续序列.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=128 lang=java
+ *
+ * [128] 最长连续序列
+ */
+
+// @lc code=start
+class Solution {
+    public int longestConsecutive(int[] nums) {
+
+    }
+}
+// @lc code=end
+
