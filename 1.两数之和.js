5e1627fa7e7fa0e06526dbe76634dadc74bf8236
mijia <1137551712@qq.com>
2022-10-21 08:20:33 +0800

    10/21/22

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -10,8 +10,28 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+// var twoSum = function(nums, target) {
+//     for (var i=0;i<nums.length-1;i++){
+//         for (var j=i+1;j<nums.length;j++){
+//             if (nums[j]+nums[i]===target){
+//                 return [i,j]
+//             }
+//         }
+//     }
+// };
 
+var twoSum = function(nums,target){
+    const map = new Map()
+    for (let i = 0;i<nums.length;i++){
+        let complement = target - nums[i];
+        if (map.has(complement)){
+            // map.has（）检查有没有key
+            return [map.get(complement),i]
+        }else{
+            map.set(nums[i],i)
+        }
+    }
+    return []
 };
 // @lc code=end
 
